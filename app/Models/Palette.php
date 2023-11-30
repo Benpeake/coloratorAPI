0f6942416be0d23b94e56937148b06be3a4b3227
Benpeake <dearbenp@gmail.com>
2023-11-30 10:15:56 +0000

    palette model and migration

---
+++ app/Models/Palette.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Palette extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $casts = [
+        'hex_colors' => 'array',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
